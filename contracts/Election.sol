e442e1bc7e52dae9913b21b837bf8216a1ea4cb1
Laura <lpedrerogallardo@outlook.es>
2018-05-23 12:01:04 +0200

    third commit and final version

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -9,11 +9,15 @@ contract Election {
     }
 
     // Store Candidate
+    mapping(address => bool) public voters;
     // Fetch Candidate
     mapping(uint => Candidate) public candidates;
 
     // Store Candidates Count
     uint public candidatesCount;
+
+    // voted event
+    event votedEvent(uint indexed _candidateId);
 
     // Constructor
     constructor () public {
@@ -24,5 +28,22 @@ contract Election {
     function addCandidate (string _name) private {
       candidatesCount ++;
       candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+    }
+
+    function vote(uint _candidateId) public{
+      //require they havent voted before
+      require(!voters[msg.sender]);
+
+      // requiere a valid candidate
+      require(_candidateId>0 && _candidateId<= candidatesCount);
+
+      //record that voter has voted
+      voters[msg.sender] = true;
+
+      //update candidate vote
+      candidates[_candidateId].voteCount ++;
+
+      // trigger voted event
+      emit votedEvent(_candidateId);
     }
 }
